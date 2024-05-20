20cbae56c7a850fa746f92418a6ce2dae85e9bb7
aryanoor <aryan.nourbakhsh@gmail.com>
2024-05-20 13:48:18 +0330

    fixing some bugs and adding notification to page

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './form.module.css'
-import { Popconfirm } from 'antd';
+import { Popconfirm, notification } from 'antd';
 import type { PopconfirmProps } from 'antd';
 
 interface User{
@@ -10,6 +10,51 @@ interface User{
     hobby: string
 }
 export default function Form() { 
+    const [user, setUser] = useState<User>({
+        firstname: "",
+        lastname: "",
+        hobby: "Swimming"
+    })
+    const [submit, setSubmit] = useState(false)
+
+    const firstnameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, firstname: e.target.value})
+    }
+    const lastnameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, lastname: e.target.value})
+    }
+    const hobbyChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setUser({...user, hobby: e.target.value})
+    }
+    const confirm: PopconfirmProps['onConfirm'] = (e) => {
+        e?.preventDefault()
+        setSubmit(true)
+      };
+      
+      const cancel: PopconfirmProps['onCancel'] = (e) => {
+        e?.preventDefault()
+        setSubmit(false)
+        notification.info({
+            message: `You have cancelled the submission`,
+            placement: 'topRight',
+        })
+      };
+
+      useEffect(() => {
+        if(submit){
+            notification.success({
+                message: `Form Submitted`,
+                description: `Thank you for submitting ${user.firstname} ${user.lastname}`,
+                placement: 'topRight',
+            })
+            setSubmit(false)
+            setUser({
+                firstname: "",
+                lastname: "",
+                hobby: "Swimming"
+            })
+        }
+      },[submit])
     
   return (
     <div className={styles.wrapper}>
@@ -17,15 +62,15 @@ export default function Form() {
         <form className={styles.form}>
             <div className={styles.input}>               
                 <label htmlFor="firstname">firstname:</label>
-                <input type="text" name="firstname" id="firstname" />
+                <input value={user.firstname} onChange={firstnameChangeHandler} type="text" name="firstname" id="firstname" />
             </div>
             <div className={styles.input}>               
                 <label htmlFor="lastname">Lastname:</label>
-                <input type="text" name="lastname" id="lastname" />
+                <input value={user.lastname} onChange={lastnameChangeHandler} type="text" name="lastname" id="lastname" />
             </div>
             <div className={styles.input}> 
                 <label htmlFor="hobbies">Favorite Hobby:</label>              
-                <select name="hobbies" id="hobbies">
+                <select onChange={hobbyChangeHandler} name="hobbies" id="hobbies">
                     <option value="Swimming">Swimming</option>
                     <option value="Codding">Codding</option>
                     <option value="Travelling">Travelling</option>
@@ -33,7 +78,16 @@ export default function Form() {
                     <option value="Playing">Playing</option>
                 </select>
             </div>
-            <button className={styles.submit} onClick={(e) => e.preventDefault()} type="submit">Submit</button>
+            <Popconfirm
+                title="Submit Form"
+                description="Are you sure to submit your form?"
+                onConfirm={confirm}
+                onCancel={cancel}
+                okText="Yes"
+                cancelText="No"
+            >
+                <button className={styles.submit} onClick={(e) => e.preventDefault()} type="submit">Submit</button>
+            </Popconfirm>
 
         </form>
     </div>
